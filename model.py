9b6e5b7b3a09426c816a52e94b802bee99cc07b2
lccatala <lccm1@alu.ua.es>
2020-07-16 17:16:38 +0000

    Dataset augmentation by flipping images

--- model.py
+++ model.py
@@ -47,25 +47,33 @@ def load_data():
     images = []
     measurements = []
     for line in lines:
-      correction = 0.2 # This is a parameter to tune
-      steering_center = float(line[3])
-      steering_left = steering_center + correction
-      steering_right = steering_center - correction
-
-      # Read in images from left, center and right cameras
-      source_path_center = line[0]
-      source_path_left = line[1]
-      source_path_right = line[2]
-
-      filename_center = source_path_center.split('/')[-1]
-      filename_left = source_path_left.split('/')[-1]
-      filename_right = source_path_right.split('/')[-1]
-
-      path_center = '../data/IMG/' + filename_center
-      path_left = '../data/IMG/' + filename_left
-      path_right = '../data/IMG/' + filename_right
-      images.extend([ndimage.imread(path_center), ndimage.imread(path_left), ndimage.imread(path_right)])
-      measurements.extend([steering_center, steering_left, steering_right])
+        correction = 0.2 # This is a parameter to tune
+        steering_center = float(line[3])
+        steering_left = steering_center + correction
+        steering_right = steering_center - correction
+        
+        # Read in images from left, center and right cameras
+        source_path_center = line[0]
+        source_path_left = line[1]
+        source_path_right = line[2]
+        
+        filename_center = source_path_center.split('/')[-1]
+        filename_left = source_path_left.split('/')[-1]
+        filename_right = source_path_right.split('/')[-1]
+        
+        path_center = '../data/IMG/' + filename_center
+        path_left = '../data/IMG/' + filename_left
+        path_right = '../data/IMG/' + filename_right
+        
+        img_center = ndimage.imread(path_center)
+        img_left = ndimage.imread(path_left)
+        img_right = ndimage.imread(path_right)
+        img_center_flipped = np.fliplr(img_center)
+        img_left_flipped = np.fliplr(img_left)
+        img_right_flipped = np.fliplr(img_right)
+      
+        images.extend([img_center, img_left, img_right, img_center_flipped, img_left_flipped, img_right_flipped])
+        measurements.extend([steering_center, steering_left, steering_right, -steering_center, -steering_left, -steering_right])
 
     X_train = np.array(images)
     y_train = np.array(measurements)
@@ -74,57 +82,47 @@ def load_data():
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, Conv2D, MaxPooling2D, Dropout
 
-def copied_model():
-    model = Sequential()
-    # Normalize and mean-center input data
-    model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
-    model.add(Lambda(lambda x: x / 255.0 - 0.5))
-    model.add(Conv2D(32, (3,3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2,2), padding='valid'))
-
-    model.add(Conv2D(32, (3,3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2,2), padding='valid'))
-
-    model.add(Conv2D(64, (3,3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2,2), padding='valid'))
-
-    model.add(Conv2D(64, (3,3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2,2), padding='valid'))
-
-    model.add(Conv2D(128, (3,3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2,2), padding='valid'))
-
-    model.add(Conv2D(128, (3,3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2,2), padding='valid'))
-
-    model.add(Flatten())
-    model.add(Dropout(0.5))
-    model.add(Dense(1024))
-    model.add(Dense(256))
-    model.add(Dense(64))
-    model.add(Dense(1))
-    
-    return model
-
 def udacity_model():
     model = Sequential()
-    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+    model.add(Lambda(lambda x: x/255.0 - 0.5))
     model.add(Conv2D(24, 5, 5, activation='relu', subsample=(2, 2)))
+    #model.add(MaxPooling2D((2,2), padding='valid'))
+    
     model.add(Conv2D(36, 5, 5, activation='relu', subsample=(2, 2)))
+    #model.add(MaxPooling2D((2,2), padding='valid'))
+    
     model.add(Conv2D(48, 5, 5, activation='relu', subsample=(2, 2)))
+    #model.add(MaxPooling2D((2,2), padding='valid'))
+    
     model.add(Conv2D(64, 3, 3, activation='relu'))
+    #model.add(MaxPooling2D((2,2), padding='valid'))
+    
     model.add(Conv2D(64, 3, 3, activation='relu'))
-    #model.add(Dropout(0.5))
+    #model.add(MaxPooling2D((2,2), padding='valid'))
+    
+    model.add(Dropout(0.5))
     model.add(Flatten())
+    
     model.add(Dense(100, activation='relu'))
     model.add(Dense(50, activation='relu'))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1))
-    #model.summary()
 
     return model
 
-EPOCHS = 5
+import matplotlib.pyplot as plt
+def plot_history(history_object, filename_fig):
+    fig = plt.figure()
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    fig.savefig(filename_fig)
+
+EPOCHS = 10
 
 if __name__ == '__main__':
     print('Loading data...')
@@ -133,6 +131,7 @@ if __name__ == '__main__':
     filename_model = 'model.h5'
     model = udacity_model()
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=EPOCHS)
+    history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=EPOCHS)
     print('Done! Saving model in', filename_model)
     model.save(filename_model)
+    plot_history(history_object, 'with_dropout.png')
